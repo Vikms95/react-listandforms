7026e0249fe81589d87a1fed24417187e8517d12
Vikms95 <vms1995@hotmail.com>
2022-05-06 03:59:21 +0200

    State is properly updated

--- react-listandforms/src/App.js
+++ react-listandforms/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super()
+    // Array of strings, only the last index gets udpated with render
+    this.state = ["hello"]
+    this.handleClick = this.renderInputValues.bind(this)
+    this.updateData = this.updateData.bind(this)
+  }
+
+  renderInputValues(){
+  // Renders all values from state and wraps them with <li> with map method
+  }
+
+  updateData(event){
+  // Gets the last index from state and renders its value to the event value
+    // const value = event.target.value
+    this.setState(prevState=>{
+      console.log(prevState)
+      const value = event.target.value
+      let newArray = Object.keys(prevState).map(key => prevState[key])
+      newArray = newArray.slice(0, newArray.length - 1)
+      return [...newArray, value]
+    })
+  }
+
+  render(){
+    return (
+      <div>
+        <form>
+            <label htmlFor="name">Name</label>
+            <input id="name" name="name" type="text" onChange={this.updateData} value={this.state.name}></input>
+
+        <button type="button">SUBMIT</button>
+        </form>
+      </div>
+    );
+   }
 }
 
 export default App;
